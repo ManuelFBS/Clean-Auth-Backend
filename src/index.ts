cd708f5d30c617b1e0e1e19ff24512c7a64c6a70
ManuelFBS <manuelf.borrego@gmail.com>
2025-06-05 18:20:23 -0400

    Actualización de 'index.ts'...

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+import dotenv from 'dotenv';
+import { createServer } from './infrastructure/web/server';
+
+dotenv.config();
+
+async function main() {
+    const app = await createServer();
+    const port = process.env.PORT || 7000;
+
+    app.listen(port, () => {
+        console.log(
+            `Server running on http://localhost:${port}`,
+        );
+    });
+}
+
+main().catch(console.error);
